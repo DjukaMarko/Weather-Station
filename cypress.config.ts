18be2fba14378030d896af2a268b3f618bdc91e3
DjukaMarko <marko.djukic.111@gmail.com>
2024-04-22 20:20:14 +0200

    bug fix

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     projectId: "13fj67",
-    baseUrl : "http://127.0.0.1:3000/",
+    baseUrl : "http://localhost:3000/",
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
